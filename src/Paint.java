5a4b8b04ad88ddc039d745768cfa65b274375b3e
Dekel <dekely96@gmail.com>
2020-04-06 16:03:37 +0300

    clip class

--- src/Paint.java
+++ src/Paint.java
@@ -3,19 +3,29 @@ import java.util.List;
 
 public class Paint {
     private Scene scene;
+    private View view;
+    private Clip clip;
+    private Line line;
 
-    public Paint(Scene sc) {
+    public Paint(Scene sc, View viw) {
         this.scene = sc;
+        this.view = viw;
+        this.clip = new Clip(this.view.getSizeX(), this.view.getSizeY());
     }
 
     public void paint(Graphics g, List<Vector> vertexList, boolean cFlag) {
         for (int i = 0; i < this.scene.getEdgesList().size(); i++) {
-            Vector v = vertexList.get(this.scene.getEdgesList().get(i).getKey());
-            Vector u = vertexList.get(this.scene.getEdgesList().get(i).getValue());
+            Vector v0 = vertexList.get(this.scene.getEdgesList().get(i).getKey());
+            Vector v1 = vertexList.get(this.scene.getEdgesList().get(i).getValue());
+            this.line = new Line(v0, v1);
             if (cFlag) {
-                //clliping
+                this.line = clip.clipLine(this.line);
+                if (this.line == null) {
+                    continue;
+                }
             }
-            g.drawLine((int)v.getX(), (int)v.getY(), (int)u.getX(), (int)u.getY());
+            g.drawLine((int)this.line.getP0().getX(), (int)this.line.getP0().getY(),
+                    (int)this.line.getP1().getX(), (int)this.line.getP1().getY());
         }
     }
 }
